b64e809b1e692da19a9afb342287f816cc52c6aa
aya <aya.7677kiki@gmail.com>
2021-06-03 10:42:05 +0900

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,162 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container">
+      <div class="todo-wrap">
+        <h2 class="title">Todo List</h2>
+        <div class="text-wrap">
+          <input
+            class="text-box"
+            type="text"
+            name="text"
+            id="text"
+            v-model="newText"
+          />
+          <button class="add-btn" @click="insertContact">追加</button>
+        </div>
+        <div class="text-wrap2" v-for="item in contactLists" :key="item.id">
+          <input class="text-box2" type="text" v-model="item.text" />
+          <div class="btn">
+            <button
+              class="update-btn"
+              @click="updateContact(item.id, item.text)"
+            >
+              更新
+            </button>
+            <button class="delete-btn" @click="deleteContact(item.id)">
+              削除
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      newText: "",
+      contactLists: [],
+    };
+  },
+  methods: {
+    async getContact() {
+      const resData = await axios.get("http://127.0.0.1:8000/api/contact/");
+      this.contactLists = resData.data.data;
+    },
+    async insertContact() {
+      const sendData = {
+        text: this.newText,
+      };
+      await axios.post("http://127.0.0.1:8000/api/contact/", sendData);
+      await this.getContact();
+    },
+    async updateContact(id, text) {
+      const sendData = {
+        text: text,
+      };
+      await axios.put("http://127.0.0.1:8000/api/contact/" + id, sendData);
+      await this.getContact();
+    },
+    async deleteContact(id) {
+      await axios.delete("http://127.0.0.1:8000/api/contact/" + id);
+      await this.getContact();
+    },
+  },
+  created() {
+    this.getContact();
+  },
+};
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  background-color: #2D197C;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
-#nav {
-  padding: 30px;
+.container {
+  width:50%;
+  margin-right: 30px;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+ .todo-wrap {
+  background-color: white;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  border-radius: 10px;
+  padding: 10px 30px 30px 30px;
 }
+.text-wrap {
+  display: flex;
+  justify-content: space-between;
+}
+.text-wrap2 {
+  display: flex;
+  justify-content: space-between;
+}
+.text-box {
+  width: 75%;
+  border: 2px solid #dfdfdf;
+  border-radius: 5px;
+  margin-bottom: 15px;
+  padding: 10px;
+}
+.text-box2 {
+  width: 40%;
+  border: 2px solid #dfdfdf;
+  border-radius: 5px;
+  padding: 10px;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+}
+.add-btn {
+  cursor: pointer;
+  background-color: white;
+  color: #dc70fa;
+  border: 3px solid;
+  border-radius: 5px;
+  margin-bottom: 15px;
+  padding: 10px 15px;
+}
+.add-btn:hover {
+  color: white;
+  background-color: #dc70fa;
+  border: 3px solid #dc70fa;
+  transition: 1.0s;
+}
+.update-btn {
+  margin-right: 5px;
+  cursor: pointer;
+  background-color: white;
+  color: #faaa93;
+  border: 3px solid;
+  border-radius: 5px;
+  padding: 10px 15px;
+}
+.update-btn:hover {
+  color: white;
+  background-color: #faaa93;
+  border: 3px solid #faaa93;
+  transition: 1.0s;
+}
+.delete-btn {
+  cursor: pointer;
+  background-color: white;
+  color: #94fae0;
+  border: 3px solid;
+  border-radius: 5px;
+  padding: 10px 15px;
+}
+.delete-btn:hover {
+  color: white;
+  background-color: #94fae0;
+  border: 3px solid #94fae0;
+  transition: 1.0s;
 }
 </style>
